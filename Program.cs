7bd738ebe2bbe6a7689be70e86dd6a29825337d3
Taylor Gulley <taylorlgulley@yahoo.com>
2019-03-17 23:20:25 -0500

    added a the solution to the Longest Word challenge

--- Program.cs
+++ Program.cs
@@ -8,5 +8,26 @@ namespace CodeChallenges
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static string LongestWord(string sen)
+        {
+            int maxWordLength = 0;
+            string maxLengthWord = "";
+            List<string> words = sen.Split(' ').ToList();
+            foreach (string word in words)
+            {
+                string parsedWord = Regex.Replace(word, @"[\W_]", string.Empty);
+                int wordLength = parsedWord.Length;
+                if (wordLength == maxWordLength)
+                {
+                }
+                else if (wordLength > maxWordLength)
+                {
+                    maxWordLength = wordLength;
+                    maxLengthWord = parsedWord;
+                }
+            }
+            return maxLengthWord;
+        }
     }
 }
