8b4044bc2c0a792cc8a2e838a9c9095a1c7510eb
Taylor Gulley <taylorlgulley@yahoo.com>
2019-03-19 22:46:13 -0500

    added comments for the longest word challenge

--- Program.cs
+++ Program.cs
@@ -13,15 +13,20 @@ namespace CodeChallenges
         // Refactor this solution to either use linq instead or remove the int because you can use .length in the if statement
         public static string LongestWord(string sen)
         {
+            // stores the longest word
             string maxLengthWord = "";
+            // splits the sentence on spaces and creates a list of the words
             List<string> words = sen.Split(' ').ToList();
             foreach (string word in words)
             {
+                // Parses each word for any characters that are not letters
                 string parsedWord = Regex.Replace(word, @"[\W_]", string.Empty);
-                if (parsedWord == maxLengthWord)
+                // This if statement sets the maxLengthWord to the last word of equal length Ex. Walk the dogs will return dogs
+                if (parsedWord.Length == maxLengthWord.Length)
                 {
                     maxLengthWord = parsedWord;
                 }
+                // sets the maxLengthWord to the parsedword if it has a greater length
                 else if (parsedWord.Length > maxLengthWord.Length)
                 {
                     maxLengthWord = parsedWord;
