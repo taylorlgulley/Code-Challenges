60f38f50968c955375129f754d0a8979db7263a8
Taylor Gulley <taylorlgulley@yahoo.com>
2019-03-31 23:16:36 -0500

    added the fizzbuzz solution

--- Program.cs
+++ Program.cs
@@ -99,5 +99,29 @@ namespace CodeChallenges
             return reverse;*/
             
           }
+
+          // A Solution to the FizzBuzz problem
+          public static string FizzBuzz() {
+          
+              for (var i = 1; i <= 100; i++)
+              {
+                    if (i % 3 = 0 && i % 5 = 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (i % 3 = 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (i % 3 = 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+              }
+          }
     }
 }
