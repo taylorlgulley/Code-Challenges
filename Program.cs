95a0850b6978459cf4d1a8102066685c2aa35343
Taylor Gulley <taylorlgulley@yahoo.com>
2019-04-04 23:55:11 -0500

    added teh solution to the CheckNums problem

--- Program.cs
+++ Program.cs
@@ -125,5 +125,21 @@ namespace CodeChallenges
                     }
               }
           }
+
+          public static string CheckNums(int num1, int num2) { 
+
+               if (num2 > num1)
+               {
+                    return "true";   
+               }
+               else if (num2 < num1)
+               {
+                   return "false";
+               }
+               else
+               {
+                    return "-1"; 
+               }
+          }
     }
 }
