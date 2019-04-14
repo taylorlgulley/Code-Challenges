1136e2a38e302c448a74c46c33320f6e5c88e653
Taylor Gulley <taylorlgulley@yahoo.com>
2019-04-13 23:15:19 -0500

    added additional solution to fizzbuzz

--- Program.cs
+++ Program.cs
@@ -124,6 +124,25 @@ namespace CodeChallenges
                         Console.WriteLine(i);
                     }
               }
+              /*
+              for (int i = 1; i <= 100; i++)  
+              {  
+                string str = "";  
+                if (i % 3 == 0)  
+                {  
+                    str += "Fizz";  
+                }  
+                if (i % 5 == 0)  
+                {  
+                    str += "Buzz";  
+                }  
+                if (str.Length == 0)  
+                {  
+                    str = i.ToString();  
+                }  
+                Console.WriteLine(str);  
+              } 
+              */
           }
 
           // A solution to the CheckNums problem
